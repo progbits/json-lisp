f4c0b9cc5279f57faa9c80481e60fb8e120effbd
progbits <53880655+progbits@users.noreply.github.com>
2022-06-12 22:14:48 +0100

    Very basic REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    // REPL.
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        stdin.read_line(&mut buffer).unwrap();
+        print!("{buffer}");
+    }
 }
